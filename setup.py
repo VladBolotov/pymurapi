cecdbea5e47f3f24913d9639a7acaaf40cadd4b6
Aleksey Balahnin <alesha961337@yandex.ru>
2023-07-19 14:23:27 +1000

    Old changes

--- setup.py
+++ setup.py
@@ -8,4 +8,4 @@ setup(name='pymurapi',
       author_email='support@robocenter.org',
       license='MIT',
       packages=['pymurapi'],
-      zip_safe=False, install_requires=['opencv-python', 'pyzmq', 'numpy', 'opencv-contrib-python'])
+      zip_safe=False, install_requires=['pyzmq', 'numpy'])
