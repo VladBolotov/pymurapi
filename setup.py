a9bf79d41ad6ac5b7310888e18e53e35c0d6bfdf
Sofia Nikiforova <bonase44@gmail.com>
2024-01-17 11:35:20 +1000

    change setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(name='pymurapi',
       version='0.0.2',
       description='murSimulator/murAUV API for python',
       url='https://github.com/murproject/pymurapi',
-      author='Vladislav Bolotov',
+      author='MUR Project',
       author_email='support@robocenter.org',
       license='MIT',
       packages=['pymurapi'],
-      zip_safe=False, install_requires=['pyzmq', 'numpy'])
+      zip_safe=False, install_requires=['opencv-python', 'pyzmq', 'numpy', 'opencv-contrib-python'])
